c33278e776c769d42a9a9ca316cc7b1401a61626
jenwiss <jenny.wisselqvist94@gmail.com>
2023-04-23 14:44:30 +0200

    First commit

---
+++ src/components/sections/HeaderLogin.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Logo from "../individuals/Logo"
+
+const HeaderLogin = () => {
+    return(
+        <section>
+            <Logo></Logo>
+        </section>
+    )
+}
+export default HeaderLogin  
+
+// less-than
+//chevron left
